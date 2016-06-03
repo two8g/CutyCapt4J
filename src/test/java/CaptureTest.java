3ea7be35b3a46c4119d9f29bdc96248e50553df4
two8g <ybxnice@gmail.com>
2016-06-03 14:24:05 +0800

    add svg test, fix lombok

--- src/test/java/CaptureTest.java
+++ src/test/java/CaptureTest.java
@@ -1,4 +1,5 @@
 import com.two8g.swissarmy.CutyCapt4J.Cutycapt;
+import com.two8g.swissarmy.CutyCapt4J.Format;
 import com.two8g.swissarmy.CutyCapt4J.Options;
 import org.junit.Test;
 
@@ -28,6 +29,30 @@ public class CaptureTest {
 
 		Cutycapt cutycapt = new Cutycapt(new File("/usr/bin/cutycapt"));
 		Options options = new Options();
+		int exitCode = cutycapt.run(options, url, out);
+		assert exitCode == 0;
+	}
+
+	@Test
+	public void testCaptureOptions() throws Exception {
+		URL url = new URL("file:/home/two8g/Develop/IdeaProjects/CutyCapt4J/src/test/resources/two8g blog.html");
+		File out = new File("target/test-classes/two8g-w720.png");
+
+		Cutycapt cutycapt = new Cutycapt(new File("/usr/bin/cutycapt"));
+		Options options = new Options();
+		options.setMinWidth(720);
+		int exitCode = cutycapt.run(options, url, out);
+		assert exitCode == 0;
+	}
+
+	@Test
+	public void testCaptureFormat() throws Exception {
+		URL url = new URL("file:/home/two8g/Develop/IdeaProjects/CutyCapt4J/src/test/resources/two8g blog.html");
+		File out = new File("target/test-classes/two8g.svg");
+
+		Cutycapt cutycapt = new Cutycapt(new File("/usr/bin/cutycapt"));
+		Options options = new Options();
+		options.setOutFormat(Format.SVG);
 		int exitCode = cutycapt.run(options, url, out);
 		assert exitCode == 0;
 	}
